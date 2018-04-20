286c8566033ecc0ba0431b48601efe9bf422a61e
baayso <baayso@outlook.com>
2018-04-20 14:43:41 +0800

    First commit.

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Fri Apr 13 12:43:36 2018
+
+@author: ChenFangjie
+
+Ploynomial Regression
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Taking care of missing data
+
+
+# Encoding categorical data
+
+
+# Avoiding the Dummy Variable Trap
+
+
+# Splitting the dataset into the Training set and Test set
+
+
+# Feature Scaling
+
+
+# Fitting Ploynomial Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Linear Regression to the Training Set
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+X_poly_2 = poly_reg.fit_transform(X_grid)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, lin_reg_2.predict(X_poly_2), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X_opt = X_poly[:, [0, 1, 2, 3, 4]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
