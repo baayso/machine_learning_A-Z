2c0aed477a9f2765400832a53214a88db521361c
baayso <baayso@outlook.com>
2018-04-27 14:44:27 +0800

    Upload upper_confidence_bound.py

---
+++ Part 6 - Reinforcement Learning/Section 25 - Upper Confidence Bound (UCB)/random_selection.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Fri Apr 27 14:20:20 2018
+
+@author: ChenFangjie
+
+Random Selection
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Implementing Random Selection
+import random
+N = 10000
+d = 10
+ads_selected = []
+total_reward = 0
+
+for n in range(0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = total_reward + reward
+
+# Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
